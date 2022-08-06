132cf739a228099869e01cb90f536fa96ab5658c
Alamin Shaikh <alaminshaikh@Alamins-MacBook-Pro.local>
2022-08-06 09:56:25 +0600

    Credentials updated

--- utils/shopify.js
+++ utils/shopify.js
@@ -1,6 +1,6 @@
 import Client from "shopify-buy";
 
 export const shopifyClient = Client.buildClient({
-  domain: "starter-test-dev.myshopify.com",
-  storefrontAccessToken: "2d2275f1352ee64b43dd1dc77219d8b6",
+  domain: "***.myshopify.com",
+  storefrontAccessToken: "***",
 });
